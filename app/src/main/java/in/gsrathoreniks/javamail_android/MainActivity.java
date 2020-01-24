c8bd7827423fed572a9a5b941a93293ada15021f
Gajendra <niks.gajsa@gmail.com>
2020-01-24 16:38:13 +0530

    initializing project

---
+++ app/src/main/java/in/gsrathoreniks/javamail_android/MainActivity.java
@@ -0,0 +1,14 @@
+package in.gsrathoreniks.javamail_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
