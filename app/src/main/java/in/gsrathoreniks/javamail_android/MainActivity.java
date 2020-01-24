aebc2cf3faedf81d3f9526769157b00fd15cc9a2
Gajendra <niks.gajsa@gmail.com>
2020-01-24 17:42:39 +0530

    added send email button

--- app/src/main/java/in/gsrathoreniks/javamail_android/MainActivity.java
+++ app/src/main/java/in/gsrathoreniks/javamail_android/MainActivity.java
@@ -2,17 +2,25 @@ package in.gsrathoreniks.javamail_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String LOG_TAG = "SendMail";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+
+    public void SendEmail(View view) {
         new Thread(new Runnable() {
 
             @Override
@@ -22,9 +30,9 @@ public class MainActivity extends AppCompatActivity {
                             "YOUR_PASSWORD");
                     sender.sendMail("EMAIL SUBJECT", "EMAIL BODY",
                             "SENDER_EMAIL@GMAIL.COM", "RECIPIENTS_EMAIL@gmail.com");
-                    Log.d("SendMail","Success");
+                    Log.d(LOG_TAG,"Success");
                 } catch (Exception e) {
-                    Log.e("SendMail", e.getMessage(), e);
+                    Log.e(LOG_TAG, e.getMessage(), e);
                 }
             }
 
