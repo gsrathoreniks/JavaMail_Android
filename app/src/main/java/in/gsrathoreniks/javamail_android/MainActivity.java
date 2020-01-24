52d1be1046e2768cfb508f3c909b5abfbe76284d
Gajendra <niks.gajsa@gmail.com>
2020-01-24 17:29:35 +0530

    activity created | all working

--- app/src/main/java/in/gsrathoreniks/javamail_android/MainActivity.java
+++ app/src/main/java/in/gsrathoreniks/javamail_android/MainActivity.java
@@ -3,6 +3,8 @@ package in.gsrathoreniks.javamail_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    GMailSender sender = new GMailSender("YOUR_EMAIL@GMAIL.COM",
+                            "YOUR_PASSWORD");
+                    sender.sendMail("EMAIL SUBJECT", "EMAIL BODY",
+                            "SENDER_EMAIL@GMAIL.COM", "RECIPIENTS_EMAIL@gmail.com");
+                    Log.d("SendMail","Success");
+                } catch (Exception e) {
+                    Log.e("SendMail", e.getMessage(), e);
+                }
+            }
+
+        }).start();
     }
 }
