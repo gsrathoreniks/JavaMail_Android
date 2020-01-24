b1b3ae13fe8d1d141a47f4b88817cbcea1d3ed81
Gajendra <niks.gajsa@gmail.com>
2020-01-24 16:56:26 +0530

    GMailSender class added
    Description : Created an Gmail sender object that will contain all the logic to send email. Here, we’re going to use GMail as SMTP server.

---
+++ app/src/main/java/in/gsrathoreniks/javamail_android/GMailSender.java
@@ -0,0 +1,59 @@
+package in.gsrathoreniks.javamail_android;
+
+import java.security.Security;
+import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class GMailSender extends javax.mail.Authenticator {
+    private String mailhost = "smtp.gmail.com";
+    private String user;
+    private String password;
+    private Session session;
+
+    static {
+        Security.addProvider(new JSSEProvider());
+    }
+
+    public GMailSender(String user, String password) {
+        this.user = user;
+        this.password = password;
+
+        Properties props = new Properties();
+        props.setProperty("mail.transport.protocol", "smtp");
+        props.setProperty("mail.host", mailhost);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.quitwait", "false");
+
+        session = Session.getDefaultInstance(props, this);
+    }
+
+    protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication(user, password);
+    }
+
+    public synchronized void sendMail(String subject, String body,
+                                      String sender, String recipients) throws Exception {
+        MimeMessage message = new MimeMessage(session);
+        DataHandler handler = new DataHandler(new ByteArrayDataSource(body.getBytes(), "text/plain"));
+        message.setSender(new InternetAddress(sender));
+        message.setSubject(subject);
+        message.setDataHandler(handler);
+
+        if (recipients.indexOf(',') > 0)
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients));
+        else
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipients));
+
+        Transport.send(message);
+    }
+}
